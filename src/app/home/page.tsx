d09c59b8cc1dde1c6d1c56f3d68d37591f8edc15
matheusabido <matheusabidux@gmail.com>
2025-03-20 11:00:10 -0400

    página de casa

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,10 +3,17 @@
 import Header from "@/components/Header"
 import { Home } from "@/types/api"
 import api from "@/utils/api"
-import { Flex, Spinner } from "@chakra-ui/react"
+import { Badge, Box, Flex, Skeleton, Spinner, Text } from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
+import Image from "next/image"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Suspense, useEffect } from "react"
+import { FaLocationDot, FaPeopleGroup } from "react-icons/fa6"
+import styles from "./page.module.css"
+import { Tooltip } from "@/components/ui/tooltip"
+import { MdBlock } from "react-icons/md"
+import { FaUserAlt } from "react-icons/fa"
+import { useAlert } from "@/providers/AlertContext"
 
 export default function HomePage() {
     return <Suspense fallback={<Flex justify="center" mt={4}><Spinner color="blue" /></Flex>}>
@@ -16,10 +23,11 @@ export default function HomePage() {
 
 function Page() {
 	const navigation = useRouter()
+	const { addAlert } = useAlert()
 	const params = useSearchParams()
 	const id = parseInt(params.get("id") || "")
 
-	const { data, isFetching, isError } = useQuery({
+	const { data, isError } = useQuery({
 		queryKey: ["home"],
 		queryFn: async () => {
 			if (!id) return await Promise.reject()
@@ -37,8 +45,47 @@ function Page() {
 		}
 	}, [id, navigation])
 
-	console.log(data)
+	useEffect(() => {
+		if (isError) addAlert({ title: "Erro!", text: "Não foi possível buscar as casas." })
+	}, [addAlert, isError])
+
 	return <>
 		<Header />
+		<Flex className={styles.info} gap={4} w="90vw" maxWidth="1600px" m="1rem auto">
+			<Box>
+				<Box shadow="md" rounded="lg" w="90vw" maxW="400px">
+					{data 
+						? <Image style={{width: "100%", objectFit: "cover", borderRadius: "inherit"}} width={400} height={400} src={`${process.env.NEXT_PUBLIC_API_URL}/home/picture?path=${encodeURIComponent(data.picture_path)}`} alt="Home picture" />
+						: <Skeleton height="10rem" />}
+				</Box>
+				<Flex justify="space-between" align="center" mt={4}>
+					<Flex gap={2} alignItems="center">
+						<FaLocationDot />
+						<Text fontWeight={500}>{data?.address}, {data?.city}</Text>
+					</Flex>
+					<Badge size="lg" bgColor="green.500" color="white" p={2} px={4}>R$ {data?.cost_day}</Badge>
+				</Flex>
+			</Box>
+			<Box shadow="lg" flex={1} p={4} w="100%">
+				<Text fontWeight={600}>Descrição</Text>
+				<Text>{data?.description}</Text>
+				<Flex display="flex" alignItems="center" gap={2} mt={8}>
+					<FaUserAlt />
+					<Text>Casa de {data?.user_name}</Text>
+				</Flex>
+				<Flex display="flex" alignItems="center" gap={2}>
+					<MdBlock />
+					<Tooltip content={data?.restriction_description} positioning={{offset: {mainAxis: 0}}} openDelay={0} closeDelay={0}>
+						<Text>Restrição: {data?.restriction_name}</Text>
+					</Tooltip>
+				</Flex>
+				<Flex display="flex" alignItems="center" gap={2}>
+					<FaPeopleGroup />
+					<Tooltip content={data?.share_type_description} positioning={{offset: {mainAxis: 0}}} openDelay={0} closeDelay={0}>
+						<Text>Tipo: {data?.share_type_name}</Text>
+					</Tooltip>
+				</Flex>
+			</Box>
+		</Flex>
 	</>
 }
